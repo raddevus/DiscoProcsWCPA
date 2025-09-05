8b0d2a36877e227914e46e8fc4a490fdcebc4f08
raddevus <1radeutsch1@gmail.com>
2025-09-05 11:12:41 -0400

    Added basic OsEnvironment class which we will continue developing.

---
+++ chpt_002/CpSysInfo/Model/Environment.cs
@@ -0,0 +1,11 @@
+namespace CpSysInfo.Model;
+
+public class OSEnvironment{
+   
+
+   /// Retrieves all environment vars and returns them as JSON 
+   public string GetAllEnvironmentVars(){
+
+   }
+}
+
