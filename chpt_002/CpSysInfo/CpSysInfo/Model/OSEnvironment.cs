f42922905c6919ed1b0039120936fd9fc6face4c
raddevus <1radeutsch1@gmail.com>
2025-09-05 16:55:34 -0400

    Cleaned up code & added comments and made it so the environment variables are sorted by name.  Also, returning the word complete so I don't confuse the reader into thinking I'm trying to return a bool when I was returned the string true

--- chpt_002/CpSysInfo/CpSysInfo/Model/OSEnvironment.cs
+++ chpt_002/CpSysInfo/CpSysInfo/Model/OSEnvironment.cs
@@ -1,13 +1,20 @@
+using System.Collections;
 namespace CpSysInfo.Model;
 
 public class OSEnvironment{
    
    /// Retrieves all environment vars and returns them as JSON 
    public string GetAllEnvironmentVars(){
-      var allEnvVars = Environment.GetEnvironmentVariables();
-      foreach (string k in allEnvVars.Keys) { 
-         Console.WriteLine($"{k} => {allEnvVars[k]}"); }
-      return "true";
+      var allEnvVars = Environment.GetEnvironmentVariables()
+         // Cast each name/value pair to a DictionaryEntry
+         .Cast<DictionaryEntry>() // This returns an IEnumerable of DictionaryEntry<string,string>
+         // sort each entry by it's Key (environment var name)
+         .OrderBy(obj => obj.Key); // This call returns an Enumerable wrapper around all the items
+
+      // Next line displays the allEnvVars type (Enumerable) & how many items are in the System.Linq.Enumerable+OrderedIterator object which is returned by the LINQ OrderBy() method 
+      Console.WriteLine($"{allEnvVars.GetType()} : {allEnvVars.Count()}");
+      foreach (DictionaryEntry k in allEnvVars) { 
+         Console.WriteLine($"{k.Key} => {k.Value}"); }
+      return "complete";
    }
 }
-
