56bb2e382b109801366d851ade3e9e727135dcee
raddevus <1radeutsch1@gmail.com>
2025-09-11 22:00:08 -0400

    Added basic ProcInfo class to get us started.

---
+++ chpt_004/CpSysInfo/CpSysInfo/Model/ProcInfo.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+
+namespace NewLibre;
+public class ProcInfo{
+    public Int32 Id{get;set;}
+    public string Name{get;set;}
+    public string Filename{get;set;}
+    public string? FileDate{get;set;}
+    public int ProcId{get;set;}
+
+    public string FileHash{get;set;}
+
+    public long FileSize{get;set;}
+    
+    public String? Created{get;set;}
+
+    public ProcInfo(String name, String filename, int procId)
+    {
+        name = name.Trim().ToLower();
+        if (!String.IsNullOrEmpty(name)){
+            var endIdx = name.IndexOf(' ');
+            if (endIdx > 0){
+                name = name.Substring(0,endIdx);
+            }
+        }
+        Name = name;
+        Filename = filename;
+        ProcId = procId;
+        Created = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        try{
+            if (Filename != null && Filename != String.Empty){
+                FileInfo fi = new FileInfo(Filename);
+                FileSize = fi.Length;
+                FileDate = fi.CreationTime.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+        }
+        catch (Exception ex){
+            Console.WriteLine($"FAIL! : {ex.GetType().ToString()}:{ex.Message}");
+            throw ex;
+        }
+    }
+}
